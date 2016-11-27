3830c287babaf931ef7417cb5bb8c3f192fc25a7
Rodrigo Rivera <rrivem@gmail.com>
2016-11-27 10:15:44 -0300

    added hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 
+const webpack = require('webpack');
+
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         app: PATHS.app
     },
@@ -14,3 +20,26 @@ module.exports = {
         filename: 'bundle.js'
     }
 };
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
