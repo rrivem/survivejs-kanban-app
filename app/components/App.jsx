0be9a9708a27357ce221963c475fd61e65d021d3
Rodrigo Rivera <rrivem@gmail.com>
2016-11-27 23:05:17 -0300

    using react component state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,23 +3,32 @@ import React from 'react';
 import Note from './Note.jsx';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
+    }
+
     render() {
-        const notes = [
-            {
-                id: uuid.v4(),
-                task: 'Learn Webpack'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Learn React'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Do laundry'
-            }
-        ];
+        const notes = this.state.notes;
         return (
             <div>
+            <button onClick={this.addNote}>+</button>
             <ul>
                 {notes.map(note =>
                     <li key={note.id}>
@@ -29,4 +38,13 @@ export default class App extends React.Component {
             </ul>
             </div>);
     }
+
+    addNote = () => {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    };
 }
