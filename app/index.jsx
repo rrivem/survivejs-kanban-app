cb6b545ad7e015c93bb32c6d8d78ce0db1a4baa3
Rodrigo Rivera <rrivem@gmail.com>
2016-11-28 11:46:20 -0300

    added support for persistence in local storage

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,11 @@
 import './main.css';
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
