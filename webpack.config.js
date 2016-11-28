64a4932ad88c27fdff3fa8f8be617b6256fdaa67
Rodrigo Rivera <rrivem@gmail.com>
2016-11-27 22:33:22 -0300

    added babel with presets

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,25 @@ const common = {
     entry: {
         app: PATHS.app
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
     },
     module: {
         loaders: [{
-            test: /\.css$/,
-            loaders: ['style', 'css'],
-            include: PATHS.app
-        }]
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: PATHS.app
+            }
+        ]
     }
 };
 
